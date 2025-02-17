810a899d8f36dfa739ebd8c434ce87fc9fbd17e8
Lali Bibilashvili <lalibibilashvili@gmail.com>
2025-02-17 22:49:57 +0100

    server js updated

--- server.js
+++ server.js
@@ -10,20 +10,39 @@ import mockData from "./seeds/mockData.js";
 const mongoURI =
   process.env.ATLAS_URI || "mongodb://127.0.0.1:27017/chess-database";
 
-const app = express();
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-  .connect(mongoURI)
+dbConnect()
   .then(async () => {
     console.log(`MongoDB connected at ${mongoURI}`);
     if (mongoURI.includes("127.0.0.1")) {
       await mockData();
     }
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("DB connection error:", err));
 
 app.use("/api/events", eventRoutes);
 
